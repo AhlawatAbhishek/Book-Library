e54b46e4346fb23098089e07ecec92ed020aaae2
AhlawatAbhishek <abhishekjobsid@pm.me>
2024-09-02 22:50:17 +0530

    chore: Update App component to use react-router-dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BooksProvider } from "./context/BooksContext";
 import Books from "./components/Books";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <BooksProvider>
-      <Books></Books>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Books />} />
+        </Routes>
+      </Router>
     </BooksProvider>
   );
 }
